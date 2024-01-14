95c33bff3a2cb4af9fb2f80f02d34e692a08b881
idoperi <idop@moveo.co.il>
2024-01-14 14:02:35 +0200

    last commit

--- src/cmps/GraphsContainer/GraphsContainer.jsx
+++ src/cmps/GraphsContainer/GraphsContainer.jsx
@@ -6,11 +6,11 @@ import { ContainerLayout } from "./styles"
 export const GraphsContainer = ({ articles }) => {
   return (
     <ContainerLayout>
-      <GraphContainer title="Dates">
-        <DatesGraph articles={articles} />
-      </GraphContainer>
       <GraphContainer title="Sources">
         <SourcesGraph articles={articles} />
+      </GraphContainer>
+      <GraphContainer title="Dates">
+        <DatesGraph articles={articles} />
       </GraphContainer>
     </ContainerLayout>
   )
